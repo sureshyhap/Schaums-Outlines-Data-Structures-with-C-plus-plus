b352dc275271772dedd1c90054add450064799eb
Suresh Yhap <sureshyhap@gmail.com>
2020-12-15 09:46:41 -0500

    Finished 2.4

--- Second/Chapter 2/Problems/4/four.cpp
+++ Second/Chapter 2/Problems/4/four.cpp
@@ -3,7 +3,13 @@
 int* add_arrays(int a[], int b[], int size);
 
 int main(int argc, char* argv[]) {
-  int a
+  int a[] = {3, 7, 5, 4, 3, 6, 1};
+  int b[] = {6, 3, 6, 1, 3, 5, 7};
+  int* sum = add_arrays(a, b, 7);
+  for (int* p = sum; p != sum + 7; ++p) {
+    std::cout << *p << ' ';
+  }
+  std::cout << '\n';
   
   return 0;
 }
